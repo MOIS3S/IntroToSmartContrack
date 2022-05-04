d67f3f41ed9b5a12ed480b1e557ced5acd811a6a
Moises Castillo <moisescastillo20@gmail.com>
2022-05-04 20:44:25 +0200

    Add comment

--- Functions.sol
+++ Functions.sol
@@ -13,14 +13,17 @@ contract Number {
         number = getNumber() * -1;
     }
 
+    //payable: La usamos cuando necesitamos dentro de una función enviar ether alguna dirección de la blockchain
     function sendEther(address payable receiver) public payable {
         receiver.transfer(msg.value);
     }
 
+    // view : La usamos para definir que una función no va modificar las variables de estado, sino que sólo las puede leer.
     function getName() public view returns (string memory) {
         return name;
     }
 
+    // pure: Se usa para definir que una función no lee ni modifica ninguna de las variables de estado y además no usa ninguna variable global
     function sum(int256 a, int256 b) public pure returns (int256 result) {
         result = a + b;
     }
